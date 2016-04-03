cace4fdcebdfa1ff96fc3ccb27e58acf7e58a80b
noahazarin <noahazarin@gmail.com>
2016-04-03 03:31:06 -0700

    Bodies now have types, which contain parts, which can be constructed. selectable races are now yaktaurs, humans, and naga

--- core/src/com/ams/character/BodyPart.java
+++ core/src/com/ams/character/BodyPart.java
@@ -10,13 +10,58 @@ package com.ams.character;
  * @author Steven
  */
 public class BodyPart {
+  // name of body part, like Left Leg
+  private final String name;
   // has a grasper, like an arm
-
-  private boolean hasGrasper;
+  private final boolean hasGrasper;
   // has a stance, like a leg
-  private boolean hasStance;
+  private final boolean hasStance;
   // contains vital organs, like a torso
-  private boolean hasVitals;
+  private final boolean hasVitals;
   // contains the creature's soul, like a brain
-  private boolean hasSoul;
+  private final boolean hasSoul;
+  
+  BodyPart(String name, boolean hasGrasper, boolean hasStance, boolean hasVitals, boolean hasSoul){
+      this.name = name;
+      this.hasGrasper = hasGrasper;
+      this.hasStance = hasStance;
+      this.hasVitals = hasVitals;
+      this.hasSoul = hasSoul;
+  }
+
+    /**
+     * @return the hasGrasper
+     */
+    public boolean isHasGrasper() {
+        return hasGrasper;
+    }
+
+    /**
+     * @return the hasStance
+     */
+    public boolean isHasStance() {
+        return hasStance;
+    }
+
+    /**
+     * @return the hasVitals
+     */
+    public boolean isHasVitals() {
+        return hasVitals;
+    }
+
+    /**
+     * @return the hasSoul
+     */
+    public boolean isHasSoul() {
+        return hasSoul;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
 }
--- core/src/com/ams/character/BodyType.java
+++ core/src/com/ams/character/BodyType.java
@@ -5,6 +5,7 @@
  */
 package com.ams.character;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -13,11 +14,74 @@ import java.util.Set;
  * @author Steven
  */
 public class BodyType {
-
+  //defined values
   List<BodyPart> bodyParts;
 
+  //calculated values
   Set<BodyPart> grasperParts;
   Set<BodyPart> stanceParts;
-  Set<BodyPart> vitalParts;
+  Set<BodyPart> vitalsParts;
   Set<BodyPart> soulParts;
+  
+  BodyType(String race){
+      this.bodyParts = new ArrayList<>();
+      switch (race) {
+          case "human":
+              this.bodyParts.add(new BodyPart("head", false, false, true, true));
+              this.bodyParts.add(new BodyPart("torso", false, false, true, false));
+              this.bodyParts.add(new BodyPart("left arm", true, false, false, false));
+              this.bodyParts.add(new BodyPart("right arm", true, false, false, false));
+              this.bodyParts.add(new BodyPart("left leg", false, true, false, false));
+              this.bodyParts.add(new BodyPart("right leg", false, true, false, false));
+              break;
+          case "yaktaur":
+              this.bodyParts.add(new BodyPart("head", false, false, true, true));
+              this.bodyParts.add(new BodyPart("upper torso", false, false, true, false));
+              this.bodyParts.add(new BodyPart("rear torso", false, false, true, false));
+              this.bodyParts.add(new BodyPart("left arm", true, false, false, false));
+              this.bodyParts.add(new BodyPart("right arm", true, false, false, false));
+              this.bodyParts.add(new BodyPart("left leg", false, true, false, false));
+              this.bodyParts.add(new BodyPart("right leg", false, true, false, false));
+              this.bodyParts.add(new BodyPart("left rear leg", false, true, false, false));
+              this.bodyParts.add(new BodyPart("right rear leg", false, true, false, false));
+              break;
+          case "naga":
+              this.bodyParts.add(new BodyPart("head", false, false, true, true));
+              this.bodyParts.add(new BodyPart("upper torso", false, false, true, false));
+              this.bodyParts.add(new BodyPart("naga tail", false, true, true, false));
+              this.bodyParts.add(new BodyPart("left arm", true, false, false, false));
+              this.bodyParts.add(new BodyPart("right arm", true, false, false, false));
+              break;
+          default:
+              this.bodyParts.add(new BodyPart("blob", false, false, true, true));
+              break;
+      }
+          
+  }
+  
+  private void recomputeBodyPartTypes() {
+    //first, clear our sets of BodyPartTypes
+    grasperParts.clear();
+    stanceParts.clear();
+    vitalsParts.clear();
+    soulParts.clear();
+    
+    //add approprate parts to sets
+    for (BodyPart bpart : bodyParts){
+        if (bpart.isHasGrasper()){
+            grasperParts.add(bpart);
+        }
+        if (bpart.isHasStance()){
+            stanceParts.add(bpart);
+        }
+        if (bpart.isHasVitals()){
+            vitalsParts.add(bpart);
+        }
+        if (bpart.isHasGrasper()){
+            soulParts.add(bpart);
+        }
+    }
+}
+  
+  
 }
